e1af82388d6bee7c6974a54d21aaecbfe5ed0fb6
theCydonian <31706764+theCydonian@users.noreply.github.com>
2018-07-30 20:21:12 -0700

    pulse

--- fauxmo.py
+++ fauxmo.py
@@ -303,17 +303,17 @@ class upnp_broadcast_responder(object):
 
             try:
                 self.ssock.bind(('',self.port))
-            except Exception, e:
+            except Exception e:
                 dbg("WARNING: Failed to bind %s:%d: %s" , (self.ip,self.port,e))
                 ok = False
 
             try:
                 self.ssock.setsockopt(socket.IPPROTO_IP,socket.IP_ADD_MEMBERSHIP,self.mreq)
-            except Exception, e:
+            except Exception e:
                 dbg('WARNING: Failed to join multicast group:',e)
                 ok = False
 
-        except Exception, e:
+        except Exception e:
             dbg("Failed to initialize UPnP sockets:",e)
             return False
         if ok:
@@ -346,7 +346,7 @@ class upnp_broadcast_responder(object):
                 return self.ssock.recvfrom(size)
             else:
                 return False, False
-        except Exception, e:
+        except Exception e:
             dbg(e)
             return False, False
 
@@ -455,7 +455,7 @@ while True:
         # Allow time for a ctrl-c to stop the process
         p.poll(100)
         time.sleep(0.1)
-    except Exception, e:
+    except Exception e:
         GPIO.cleanup()
         dbg(e)
         break
